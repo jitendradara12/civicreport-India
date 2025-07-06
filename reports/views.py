63a3ae943556108c69e5efded07f98b705dc26ab
sastauser <jitendradara4464@gmail.com>
2025-07-06 19:45:13 +0530

    basic frontend

--- reports/views.py
+++ reports/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from .models import InfrastructureReport
 
 class HomePageView(TemplateView):
     template_name = "reports/home.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'CivicReport India'
+        context.update({
+            'total_reports': InfrastructureReport.objects.count(),
+            'resolved_reports': InfrastructureReport.objects.filter(status='RES').count(),
+            'recent_reports': InfrastructureReport.objects.order_by('-reported_at')[:5]
+        })
         return context
